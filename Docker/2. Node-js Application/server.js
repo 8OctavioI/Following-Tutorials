5f3735619e5399d16eef04020aa89e8773bd5763
8octavioi <8octavioi@gmail.com>
2024-07-27 10:15:57 +0100

    Practice Docker Compose on Node-js Application

--- Docker/2. Node-js Application/server.js
+++ Docker/2. Node-js Application/server.js
@@ -1,10 +1,19 @@
 var http = require("http");
 
+
+
+
 http.createServer(function (request, response) {    
+    let ts = Date.now();
+    
+    let date_ob = new Date(ts);
+    let date = date_ob.getSeconds();
+    let month = date_ob.getMinutes();
+    let year = date_ob.getHours();
     response.writeHead(200, {'Content-Type': 'text/plain'});
-     
-    response.end('We are running from within a container\n');
-    console.log('The server is running from within a container');
+    response.end('Hello, We are running from within a container\n' + 'The time is: ' + year + "-" + month + "-" + date);
+    console.log('The server is running from within a container for ');
+
  }).listen(80);
  
   
