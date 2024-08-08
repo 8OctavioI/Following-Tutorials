6fde695cdd156dd48c0a15e0d7e11a29bf4cac7f
8octavioi <8octavioi@gmail.com>
2024-08-08 14:50:27 +0100

    Project 1 v2

--- Java/1. Simple Application/MortgageCalculator.java
+++ Java/1. Simple Application/MortgageCalculator.java
@@ -27,6 +27,19 @@ class MortgageCalculator {
 
     public static void run() {
 
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        final int MIN_PRINCIPAL = 1_000;
+        final int MAX_PRINCIPAL = 1_000_000;
+
+        final byte MIN_INTEREST_RATE = 0;
+        final byte MAX_INTEREST_RATE = 30;
+
+        final byte MIN_TERM = 1;
+        final byte MAX_TERM = 30;
+
+
         System.out.println("Mortgage Calculator!");
 
         Scanner scanner = new Scanner(System.in);
@@ -35,7 +48,7 @@ class MortgageCalculator {
         System.out.print("Principal: ");
 
         String temp = scanner.nextLine();
-        while(!(isNumeric(temp) && Integer.parseInt(temp) >= 1_000 && Integer.parseInt(temp) <= 1_000_000)) {
+        while(!(isNumeric(temp) && Integer.parseInt(temp) >= MIN_PRINCIPAL && Integer.parseInt(temp) <= MAX_PRINCIPAL)) {
             System.out.print("Invalid Amount. Please input a number between 1,000 and 1,000,000. \nPrincipal: ");
             temp = scanner.nextLine();
         }
@@ -44,22 +57,22 @@ class MortgageCalculator {
         System.out.print("Annual Interest Rate: ");
 
         temp = scanner.nextLine();
-        while(!(isDouble(temp) && Double.parseDouble(temp) > 0 && Double.parseDouble(temp) <= 30)) {
+        while(!(isDouble(temp) && Double.parseDouble(temp) > MIN_INTEREST_RATE && Double.parseDouble(temp) <= MAX_INTEREST_RATE)) {
             System.out.print("Invalid Amount. Please input a number between 0 and 30. \nAnnual Interest Rate: ");
             temp = scanner.nextLine();
         }
-        double annualInterestRate = Double.parseDouble(temp)/100;
-        double monthlyInterestRate = annualInterestRate/12;
+        double annualInterestRate = Double.parseDouble(temp) / PERCENT;
+        double monthlyInterestRate = annualInterestRate / MONTHS_IN_YEAR;
 
         System.out.print("Period (years): ");
 
         temp = scanner.nextLine();
-        while(!(isNumeric(temp) && Integer.parseInt(temp) > 0 && Integer.parseInt(temp) <= 30)) {
+        while(!(isNumeric(temp) && Integer.parseInt(temp) >= MIN_TERM && Integer.parseInt(temp) <= MAX_TERM)) {
             System.out.print("Invalid Amount. Please input a number between 1 and 30. \nPeriod (years): ");
             temp = scanner.nextLine();
         }
         double term = Double.parseDouble(temp);
-        double numOfPayments = term * 12;
+        double numOfPayments = term * MONTHS_IN_YEAR;
 
         String mortgage = NumberFormat.getCurrencyInstance().format(principal * 
                                                                     (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numOfPayments)) / 
